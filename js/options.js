3d58bf8f8a92358cfc56ff2f41c4afe2916d7e06
redmorrayne <redmorrayne@gmail.com>
2024-09-30 18:42:17 +0300

    Add files via upload

--- js/options.js
+++ js/options.js
@@ -1,25 +1,241 @@
-function optionsBtnSwitcher(width, holder, index) {
-    holder.children[0].style.width = width[index] + "px";
-    if (index === 0) {
-        holder.children[0].style.transform = "translate(0px)";
-    } else {
-        let btnWidth = 0;
-        for (var j = 0; j < index; j++) { 
-            btnWidth += width[j];
-        }
-        holder.children[0].style.transform = "translate(" + (btnWidth + 8 * index) + "px)";
-    }
+// LET VAR CONST
+
+const optGap = 12;
+const darkColors = [
+  "#2F4F4F", // Dark Slate Gray
+  "#4B0082", // Indigo
+  "#8B4513", // Saddle Brown
+  "#556B2F", // Dark Olive Green
+  "#2E8B57", // Sea Green
+  "#483D8B", // Dark Slate Blue
+  "#5F9EA0", // Cadet Blue
+  "#7B68EE", // Medium Slate Blue
+  "#8B0000", // Dark Red
+  "#4A4A4A", // Dim Gray
+  "#696969", // Dim Gray
+  "#8B8C7A", // Dark Gray
+  "#3C3F3F", // Dark Charcoal
+  "#4E5452", // Steel Gray
+  "#6B5B4D", // Brown Sugar
+  "#8FBC8F", // Dark Sea Green
+  "#7E7B7A", // Ash Gray
+  "#2E2E2E", // Jet Black
+  "#4B3D3D", // Charcoal Brown
+  "#4F4F4F", // Charcoal Gray
+  "#3B3B3B", // Onyx
+];
+const lightColors = [
+  "#F0E68C", // Khaki
+  "#FFE4B5", // Moccasin
+  "#FFDAB9", // Peach Puff
+  "#FFB6C1", // Light Pink
+  "#E6E6FA", // Lavender
+  "#D3D3D3", // Light Gray
+  "#FFFACD", // Lemon Chiffon
+  "#FFF0F5", // Lavender Blush
+  "#F5F5DC", // Beige
+  "#F5FFFA", // Mint Cream
+  "#FAFAD2", // Light Goldenrod Yellow
+  "#E0FFFF", // Light Cyan
+  "#FFDEAD", // Navajo White
+  "#F0FFF0", // Honeydew
+  "#B0E0E6", // Powder Blue
+  "#ADD8E6", // Light Blue
+  "#FFEFD5", // Papaya Whip
+  "#F0E68C", // Khaki
+  "#FFE4E1", // Misty Rose
+  "#D8BFD8", // Thistle
+  "#FFFAF0", // Floral White
+];
+let widths = Array(document.querySelectorAll(".opt").length).fill().map(() => []);
+
+const maleArray = ["m0.svg", "m1.svg", "m2.svg", "m3.svg", "m4.svg", "m5.svg", "m6.svg", "m7.svg"];
+const femaleArray = ["f0.svg", "f1.svg", "f2.svg", "f3.svg", "f4.svg", "f5.svg", "f6.svg", "f7.svg"];
+const iconArray = maleArray.concat(femaleArray);
+const rusLang = [];
+const engLang = [];
+
+
+
+
+
+// FUNCTIONS
+
+function changeTheme(a, b, c, d, e, f) {
+  document.body.style.setProperty("--main", a);
+  document.body.style.setProperty("--extra", b);
+  document.body.style.setProperty("--third", c);
+  document.body.style.setProperty("--text", d);
+  document.body.style.setProperty("--outline", e);
+  document.body.style.setProperty("--placeholder-color", f);
 }
-document.querySelectorAll(".theme-btn-holder").forEach(function(el) {
-    let widthArray = [];
-    let realIndex = 0;
-    el.addEventListener("click", function() {
-        for (var i = 1; i < el.parentElement.children.length; i++) {
-            widthArray.push(el.parentElement.children[i].clientWidth);
-            if (el.parentElement.children[i] === el) {
-                realIndex = i - 1;
-            }
-        }
-        optionsBtnSwitcher(widthArray, el.parentElement.children[0], realIndex);
-    })
-})
+function getRandomColors() {
+  var chosenValue = Math.random() < 0.5 ? 0 : 1;
+  let selectedArray = [];
+  if (chosenValue === 1) {
+    selectedArray = lightColors;
+  } else {
+    selectedArray = darkColors;
+  }
+  const selectedColors = [];
+  while (selectedColors.length < 3) {
+    const color =
+      selectedArray[Math.floor(Math.random() * selectedArray.length)];
+    if (!selectedColors.includes(color)) {
+      selectedColors.push(color);
+    }
+  }
+  selectedColors.push(chosenValue);
+  return selectedColors;
+}
+function moveToChecked(formIndex, labelIndex) {
+  const el = document.querySelectorAll(".opt").item(formIndex);
+  el.children[0].style.width = widths[formIndex][labelIndex] + "px";
+  el.children[0].style.outline = "solid 1px #0da32b";
+  if (labelIndex === 0) {
+    el.children[0].style.transform = "";
+  } else {
+    let left = 0;
+    for (var i = 0; i < labelIndex; i++) {
+      left += widths[formIndex][i];
+    }
+    left += labelIndex * optGap;
+    el.children[0].style.transform = "translate(" + left + "px)";
+  }
+  el.children[0].style.outline = "solid 1px #0da32b";
+  setTimeout(() => {
+    el.children[0].style.outline = "";
+  }, 400);
+}
+function themeSwitcher(labelIndex) {
+  if (labelIndex === 0) {
+    changeTheme(
+      "#010409",
+      "#0d1117",
+      "#010409",
+      "#ffffff",
+      "#ffffff20",
+      "#ffffff60"
+    );
+    document.querySelectorAll(".svg").forEach(function (el) {
+      el.style.filter = "";
+    });
+  } else if (labelIndex === 1) {
+    changeTheme(
+      "#ffffff",
+      "#e5e5e5",
+      "#ffffff",
+      "#000000",
+      "#000000",
+      "#00000060"
+    );
+    document.querySelectorAll(".svg").forEach(function (el) {
+      el.style.filter = "invert(0)";
+    });
+  } else if (labelIndex === 2) {
+    changeTheme(
+      "#AD66D5",
+      "#4575D4",
+      "#05296E",
+      "#ffffff",
+      "#ffffff",
+      "#ffffff90"
+    );
+    document.querySelectorAll(".svg").forEach(function (el) {
+      el.style.filter = "invert(1)";
+    });
+  } else {
+    const random = getRandomColors();
+    if (random[3] === 1) {
+      changeTheme(
+        random[0],
+        random[1],
+        random[2],
+        "#000000",
+        "#000000",
+        "#00000060"
+      );
+      document.querySelectorAll(".svg").forEach(function (el) {
+        el.style.filter = "";
+      });
+    } else {
+      changeTheme(
+        random[0],
+        random[1],
+        random[2],
+        "#ffffff",
+        "#ffffff",
+        "#ffffff60"
+      );
+      document.querySelectorAll(".svg").forEach(function (el) {
+        el.style.filter = "invert(1)";
+      });
+    }
+  }
+}
+function random(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+function pickGender(labelIndex) {
+  let icon = document.querySelector(".top-row-svg");
+  let selectedIcon;
+  if (labelIndex === 0) {
+    selectedIcon = random(iconArray);
+  } else if (labelIndex === 1) {
+    selectedIcon = random(maleArray);
+  } else {
+    selectedIcon = random(femaleArray);
+  }
+  localStorage.setItem("icon", selectedIcon);
+  icon.setAttribute("src", "../res/icons/" + selectedIcon);
+}
+// function languageSwap() {
+
+// }
+
+// CODE
+
+document.querySelectorAll(".opt").forEach(function (el, index) {
+  for (var i = 2; i < el.children.length; i += 2) {
+    widths[index].push(el.children[i].clientWidth);
+  }
+});
+document.addEventListener("click", function (event) {
+  if (event.target.classList.contains("opt-label")) {
+    const formIndex = Array.from(event.target.parentElement.parentElement.parentElement.querySelectorAll(event.target.parentElement.tagName)).indexOf(event.target.parentElement);
+    let labelIndex = Array.from(event.target.parentElement.querySelectorAll("label")).indexOf(event.target);
+    moveToChecked(formIndex, labelIndex);
+    if (formIndex === 0) {
+      themeSwitcher(labelIndex);
+      localStorage.setItem("theme", labelIndex);
+    } else if (formIndex === 1) {
+      pickGender(labelIndex);
+    } else if (formIndex === 2) {
+
+    }
+  }
+});
+
+// SAVE AND SAVE LOAD 
+
+if (localStorage.getItem("theme")) {
+  themeSwitcher(Number(localStorage.getItem("theme")));
+  moveToChecked(0, localStorage.getItem("theme"));
+} else {
+  moveToChecked(0, 0);
+} if (localStorage.getItem("icon")) {
+  document.querySelector(".top-row-svg").setAttribute("src", "../res/icons/" + localStorage.getItem("icon"));
+  if (localStorage.getItem("icon").slice(0, 1) == "m") {
+    moveToChecked(1, 1);
+  } else {
+    moveToChecked(1, 2);
+  }
+} else {
+  moveToChecked(1, 0);
+} if (localStorage.getItem("lang")) {
+  languageSwap();
+  moveToChecked(2, localStorage.getItem("lang"));
+} else {
+  moveToChecked(2, 0);
+}
+
--- js/toprow.js
+++ js/toprow.js
@@ -1,7 +1,67 @@
-const way = "./res/icons/";
-const icons = ["f0", "f1", "f2", "f3", "f4", "f5", "f6", "f7", "m0", "m1", "m2", "m3", "m4", "m5", "m6", "m7"];
-function getRandomIcon() {
-    const randomIndex = Math.floor(Math.random() * icons.length);
-    return way + icons[randomIndex] + ".svg";
+// STREAK
+
+function formatDate(date) {
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = date.getFullYear();
+  return day + month + year;
 }
-document.querySelector(".toprow-icon").src = getRandomIcon();
+function keapStreak() {
+  const date = new Date();
+  const today = formatDate(date);
+  const yesterdayDate = new Date(date);
+  yesterdayDate.setDate(date.getDate() - 1);
+  const yesterday = formatDate(yesterdayDate);
+  const lastVisit = localStorage.getItem("lastVisit");
+
+  if (!lastVisit) {
+    localStorage.setItem("lastVisit", today);
+    console.log("streak lost");
+    return 0;
+  }
+  if (today === lastVisit) {
+    localStorage.setItem("lastVisit", today);
+    console.log("already been today");
+    return 2;
+  } else if (yesterday === lastVisit) {
+    localStorage.setItem("lastVisit", today);
+    console.log("streak updated");
+    return 1;
+  } else {
+    localStorage.setItem("lastVisit", today);
+    console.log("streak lost");
+    return 0;
+  }
+}
+
+let streak = Number(localStorage.getItem("streak")) || 0;
+let icon = document.getElementById("top-row-streak-holder").children[0].children[0];
+const judge = keapStreak();
+
+if (judge === 0) {
+  streak = 0;
+}
+if (judge === 1) {
+  streak += 1;
+}
+if (streak === 0) {
+    icon.setAttribute("src", "./res/svg/streak/nostreak.svg");
+    document.getElementById("streak-holder").style.background = "";
+} else if (streak < 10){
+    icon.setAttribute("src", "./res/svg/streak/streak.svg");
+} else {
+    icon.setAttribute("src", "./res/svg/streak/streak.svg");
+    document.getElementById("streak-holder").style.background = "linear-gradient(-45deg, #ff7300, #ff0000)";
+}
+
+document.getElementById("top-row-streak-holder").children[1].textContent = streak;
+localStorage.setItem("streak", streak);
+
+// SINCE
+
+if (localStorage.getItem("since") === null) {
+  localStorage.setItem("since", formatDate(new Date()).slice(0, 2) + "." + formatDate(new Date()).slice(2, 4) + "." + formatDate(new Date()).slice(4, 9));
+  console.log("registration date set");
+}
+const since = localStorage.getItem("since");
+document.getElementById("top-row-text-holder").children[1].textContent = "since: " + since;
