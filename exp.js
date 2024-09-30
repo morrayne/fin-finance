ad350bb5b168bc28f9cb2be317ba02c895f98a89
redmorrayne <redmorrayne@gmail.com>
2024-09-30 22:06:29 +0300

    Delete exp.js

--- exp.js
+++
@@ -1,8 +0,0 @@
-var _a;
-var record1;
-record1 = {
-    ename: "Расход 1",
-    esum: 40000,
-    ecl: 1
-};
-console.log((_a = document.getElementById("top-row-streak-holder")) === null || _a === void 0 ? void 0 : _a.children[1].textContent);
