e71dcd7b94a3f51df8e6474770deb2c0b912d8bb
redmorrayne <redmorrayne@gmail.com>
2024-10-02 00:11:29 +0300

    Add files via upload

--- js/exp.js
+++ js/exp.js
@@ -1,21 +1,68 @@
-var record1;
-record1 = {
-    ename: "Расход 1",
-    esum: 40000,
-    ecl: 1,
-};
+// type expRecord = {
+//   ename: string;
+//   esum: number;
+//   ecl: number;
+// };
+// let record1: expRecord;
+// record1 = {
+//   ename: "Расход 1",
+//   esum: 40000,
+//   ecl: 1,
+// };
 var formElement = document.getElementById("create-exp");
 formElement.addEventListener("submit", sendExp);
 function sendExp(event) {
     event.preventDefault();
-    var lastChild = event.target.lastElementChild;
-    if (lastChild.children[1].classList.contains("ready-submit")) {
-        console.log("may");
+    var target = event.target;
+    var lastChild = target.lastElementChild;
+    var secondChild = lastChild.children[1];
+    if (secondChild.classList.contains("ready-submit")) {
+        // console.log(target.querySelectorAll(".exp-input"));
+        var inputValues = [];
+        var inputs = target.querySelectorAll(".exp-input");
+        for (var i = 0; i < inputs.length; i++) {
+            inputValues.push(inputs.item(i).value);
+        }
+        var radioButtons = target.querySelectorAll(".form-wrap input[type='radio']");
+        var checkedRadioValue = null;
+        for (var i = 0; i < radioButtons.length; i++) {
+            if (radioButtons.item(i).checked) {
+                checkedRadioValue = i;
+                break;
+            }
+        }
+        console.log(inputValues);
+        console.log(checkedRadioValue);
     }
     else {
-        lastChild.children[1].style.outline = "solid 1px #ff0000";
+        secondChild.style.outline = "solid 1px #ff0000";
         setTimeout(function () {
-            lastChild.children[1].style.outline = "";
+            secondChild.style.outline = "";
         }, 400);
     }
 }
+function dayTop(date) {
+    var day = document.createElement("button");
+    day.classList.add("day");
+    var daytop = document.createElement("div");
+    daytop.classList.add("day-top");
+    var span1 = document.createElement("span");
+    span1.textContent = "date";
+    var span2 = document.createElement("span");
+    span1.textContent = "summ";
+    daytop.appendChild(span1);
+    daytop.appendChild(span2);
+    day.appendChild(daytop);
+}
+function dayBot(date, inputValues, checkedRadioValue) {
+    // if (recordExist) {
+    // } else {
+    // }
+}
+function recordExist(date) {
+    var _a;
+    var day = (_a = document.getElementById("exp-list")) === null || _a === void 0 ? void 0 : _a.querySelectorAll(".day");
+    return day;
+}
+var days = recordExist("some-date");
+console.log(days[0].children[0].children[0].textContent, days[1].children[0].children[0].textContent);
--- js/exp.ts
+++ js/exp.ts
@@ -1,30 +1,78 @@
-type expRecord = {
-  ename: string;
-  esum: number;
-  ecl: number;
-};
+// type expRecord = {
+//   ename: string;
+//   esum: number;
+//   ecl: number;
+// };
 
-let record1: expRecord;
+// let record1: expRecord;
 
-record1 = {
-  ename: "Расход 1",
-  esum: 40000,
-  ecl: 1,
-};
+// record1 = {
+//   ename: "Расход 1",
+//   esum: 40000,
+//   ecl: 1,
+// };
 
 const formElement = document.getElementById("create-exp") as HTMLFormElement;
-
 formElement.addEventListener("submit", sendExp);
 
-function sendExp(event): void {
+function sendExp(event: Event): void {
   event.preventDefault();
-  const lastChild = event.target.lastElementChild as HTMLElement;
-  if (lastChild.children[1].classList.contains("ready-submit")) {
-    console.log("may");
+  const target = event.target as HTMLFormElement;
+  const lastChild = target.lastElementChild as HTMLElement;
+
+  const secondChild = lastChild.children[1] as HTMLElement;
+  if (secondChild.classList.contains("ready-submit")) {
+    // console.log(target.querySelectorAll(".exp-input"));
+    let inputValues: string[] = [];
+    const inputs = target.querySelectorAll<HTMLInputElement>(".exp-input");
+    for (let i = 0; i < inputs.length; i++) {
+      inputValues.push(inputs.item(i).value);
+    }
+    const radioButtons = target.querySelectorAll<HTMLInputElement>(
+      ".form-wrap input[type='radio']"
+    );
+    let checkedRadioValue: number | null = null;
+    for (let i = 0; i < radioButtons.length; i++) {
+      if (radioButtons.item(i).checked) {
+        checkedRadioValue = i;
+        break;
+      }
+    }
+    console.log(inputValues);
+    console.log(checkedRadioValue);
   } else {
-    lastChild.children[1].style.outline = "solid 1px #ff0000";
+    secondChild.style.outline = "solid 1px #ff0000";
     setTimeout(() => {
-      lastChild.children[1].style.outline = "";
+      secondChild.style.outline = "";
     }, 400);
   }
 }
+
+function dayTop(date: string) {
+  let day = document.createElement("button");
+  day.classList.add("day");
+  let daytop = document.createElement("div");
+  daytop.classList.add("day-top");
+  let span1 = document.createElement("span");
+  span1.textContent = "date";
+  let span2 = document.createElement("span");
+  span1.textContent = "summ";
+  daytop.appendChild(span1);
+  daytop.appendChild(span2);
+  day.appendChild(daytop);
+}
+function dayBot(date: string, inputValues: string, checkedRadioValue: number) {
+  // if (recordExist) {
+
+  // } else {
+
+  // }
+}
+
+function recordExist(date: string) {
+  let day = document.getElementById("exp-list")?.querySelectorAll(".day");
+  return day;
+}
+
+let days = recordExist("some-date");
+console.log(days[0].children[0].children[0].textContent, days[1].children[0].children[0].textContent);
